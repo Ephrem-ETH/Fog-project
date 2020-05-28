699f498c90f4d756f75af09ee975cca26b15fe8a
Luca <luca.fregolon@studenti.unitn.it>
2020-05-28 13:27:33 +0000

    Reorganized folders + updated #nodes

--- Environment/prepareEnv.sh
+++ Environment/prepareEnv.sh
@@ -4,13 +4,13 @@ kubectl apply -f persistentVolumes/pvc_web.yaml
 kubectl apply -f persistentVolumes/pv_mysql.yaml
 kubectl apply -f persistentVolumes/pvc_mysql.yaml
 kubectl apply -f php/php_conf.yaml
-kubectl apply -f php/php.yaml
+kubectl apply -f php/php.yaml --record
 php_svc=`kubectl get svc | awk '/php-service/ {print $3}'`
 template=`cat nginx/nginx_conf.yaml | sed "s/{{php_svc}}/$php_svc/g"`
 echo "$template" | kubectl apply -f -
-kubectl apply -f nginx/nginx.yaml
+kubectl apply -f nginx/nginx.yaml --record
 kubectl apply -f mysql/mysql_conf.yaml
-kubectl apply -f mysql/mysql.yaml
+kubectl apply -f mysql/mysql.yaml --record
 echo "PHP listening on $php_svc:9000"
 mysql_svc=`kubectl get svc | awk '/mysql-service/ {print $3}'`
 echo "MYSQL listening on $mysql_svc:3306"
